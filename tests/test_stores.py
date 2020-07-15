5642f663a28bb4e0e66fc47bb8cd1dea8d68898e
msdcanderson <michael.anderson@banksearch-consultancy.com>
2020-07-15 15:46:23 +0100

    create tests for stores

--- tests/test_stores.py
+++ tests/test_stores.py
@@ -20,12 +20,12 @@ def test_post_store_succeeds(test_client):
     }
     mock_request_data = {
 	"name": "Store 1",
-	"description": "This is store 1",
+	"description": "Store 1",
     }
-    res = test_client.post('/store', data=json.dumps(mock_request_data), headers=mock_request_headers)
-    assert res.status_code == 201
+    response = test_client.post('/store', data=json.dumps(mock_request_data), headers=mock_request_headers)
+    assert response.status_code == 201
     expected = {'message': 'STORE_CREATED'}
-    assert expected == json.loads(res.get_data(as_text=True))
+    assert expected == json.loads(response.get_data(as_text=True))
 
 
 def test_patch_store_succeeds(test_client):
@@ -33,17 +33,17 @@ def test_patch_store_succeeds(test_client):
         'Content-Type': 'application/json'
     }
     mock_request_data = {
-   	"name": "store 1A",
-	"description": "This is store 1A",
+   	"name": "Store 1A",
+	"description": "Store 1A",
     }
-    res = test_client.patch('/store/1', data=json.dumps(mock_request_data), headers=mock_request_headers)
-    assert res.status_code == 200
+    response = test_client.patch('/store/1', data=json.dumps(mock_request_data), headers=mock_request_headers)
+    assert response.status_code == 200
     expected = {
         "id": 1,
-        "name": "store 1A",
-        "description": "This is store 1A"
+        "name": "Store 1A",
+        "description": "Store 1A"
     }
-    assert expected == json.loads(res.get_data(as_text=True))
+    assert expected == json.loads(response.get_data(as_text=True))
 
 
 def test_patch_store_not_found(test_client):
@@ -51,12 +51,74 @@ def test_patch_store_not_found(test_client):
         'Content-Type': 'application/json'
     }
     mock_request_data = {
-   	"name": "store 1A",
-	"description": "This is store 1A",
+   	"name": "Store 1A",
+	"description": "Store 1A",
     }
-    res = test_client.patch('/store/2', data=json.dumps(mock_request_data), headers=mock_request_headers)
-    assert res.status_code == 404
+    response = test_client.patch('/store/2', data=json.dumps(mock_request_data), headers=mock_request_headers)
+    assert response.status_code == 404
     expected = {'message': 'STORE_NOT_FOUND'}
-    assert expected == json.loads(res.get_data(as_text=True))
+    assert expected == json.loads(response.get_data(as_text=True))
+
+
+def test_get_stores(test_client):
+    """
+    Test getting list of Stores
+    """
+    response = test_client.get('/stores')
+    assert response.status_code == 200
+    expected = {
+        "stores": [
+            {
+                "id": 1,
+                "name": "Store 1A",
+                "description": "Store 1A",
+            }
+        ]
+    }
+    assert expected == json.loads(response.get_data(as_text=True))
+
+
+def test_get_store(test_client):
+    """
+    Test getting a single store
+    """
+    response = test_client.get('/store/1')
+    assert response.status_code == 200
+    expected = {
+        "id": 1,
+        "name": "Store 1A",
+        "description": "Store 1A"
+    }
+    assert expected == json.loads(response.get_data(as_text=True))
+
+
+def test_get_store_not_found(test_client):
+    """
+    Test getting a single store that doesn't exist
+    """
+    response = test_client.get('/store/2')
+    assert response.status_code == 404
+    expected = {"message": "STORE_NOT_FOUND"}
+    assert expected == json.loads(response.get_data(as_text=True))
+
+
+def test_delete_store(test_client):
+    """
+    Test Store 1 is deleted from DB
+    """
+    response = test_client.delete('/store/1')
+    assert response.status_code == 200
+    expected = {"message": "STORE_DELETED"}
+    assert expected == json.loads(response.get_data(as_text=True))
+
+
+def test_delete_store_not_found(test_client):
+    """
+    Test deletion of a Store that doesn't exist
+    """
+    response = test_client.delete('/store/1')
+    assert response.status_code == 404
+    expected = {"message": "STORE_NOT_FOUND"}
+    assert expected == json.loads(response.get_data(as_text=True))
 
 
