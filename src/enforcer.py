0f97e093df97e26738aa37d3b14a06846228564f
msdcanderson <michael.anderson@banksearch-consultancy.com>
2020-07-30 18:11:27 +0100

    update enforcer

--- src/enforcer.py
+++ src/enforcer.py
@@ -1,4 +1,5 @@
 import sys
+import casbin
 
 sys.path.append("../../flask-authz")
 from flask_authz_mine import CasbinEnforcer
@@ -9,11 +10,27 @@ class UpdatedCasbinEnforcer(CasbinEnforcer):
         self.app = app
         self.adapter = adapter
         if app is not None and adapter is not None:
-            self.init_app(app, adapter)
+            self.init_app(app, adapter, watcher)
 
-    def init_app(self, app, adapter):
+    def init_app(self, app, adapter, watcher=None):
         self.app = app
         self.adapter = adapter
-        self.e = CasbinEnforcer(app.config.get("CASBIN_MODEL"), self.adapter, True)
+        self.e = casbin.Enforcer(app.config.get("CASBIN_MODEL"), self.adapter, True)
         if watcher:
             self.e.set_watcher(watcher)
+
+
+
+# class UpdatedCasbinEnforcer(CasbinEnforcer):
+# 	def __init__(self, app=None, adapter=None, watcher=None):
+# 		self.app = app
+# 		self.adapter = adapter
+#         if app is not None and adapter is not None:
+#             self.init_app(app, adapter, watcher)
+# ​
+# 	def init_app(self, app, adapter, watcher=None):
+# 		self.app = app
+# 		self.adapter = adapter
+# 		self.e = casbin.Enforcer(app.config.get("CASBIN_MODEL"), self.adapter, True)
+# 		if watcher:
+# 			self.e.set_watcher(watcher)
--- src/main/resources/store.py
+++ src/main/resources/store.py
@@ -3,7 +3,7 @@ from flask import request
 from flask_babel import gettext
 from flask_login import login_required, current_user
 from werkzeug.exceptions import NotFound, Unauthorized
-# from app import casbin_enforcer
+from extensions import casbin_enforcer
 # from extensions import authorize
 
 from main.models.store import StoreModel
@@ -33,7 +33,7 @@ class NewStore(Resource):
 class Store(Resource):
     @classmethod
     @login_required
-    # @casbin_enforcer.enforcer
+    @casbin_enforcer.enforcer
     def get(cls, _id: int):
         store = StoreModel.find_by_id(_id)
         if not store:
